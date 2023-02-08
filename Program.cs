ac11683c08d25f1cba745fb3f0a76e8834f7949b
Nadya <nadya0707@yandex.ru>
2023-02-09 01:09:24 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void Release(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum = (sum + matrix[i, j]);
+    }
+    sum = sum / n;
+    Console.WriteLine($"{Math.Round(sum,2)}");
+}
+}
+Release(matrix);
